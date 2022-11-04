35cf97ef7fd4ecf9e1a7ccdd1453ce2707e6d0fc
Jahid Hasan <114799772+JahidHasan93@users.noreply.github.com>
2022-11-04 12:53:36 +0600

    deposite updated

--- bank.js
+++ bank.js
@@ -4,7 +4,47 @@ enterButton.addEventListener("click",function(){
     loginArea.style.display="none"
     const mainArea = document.getElementById("main-area");
     mainArea.style.display="block"
-    const transactionArea=document.getElementById("transaction-area");
-    transactionArea.style.display="block"
+   const transactionArea=document.getElementById("transaction-area");
+   transactionArea.style.display="block"
+})
 
+//transaction
+const depositeButton = document.getElementById("deposite-button");
+depositeButton.addEventListener("click",function(){
+    const depositeAmount= document.getElementById("deposite-amount").value;
+    const depositeNumber=parseFloat(depositeAmount);
+
+    const currentBalance= document.getElementById("current-balance").innerText;
+    const currentAmount= parseFloat(currentBalance);
+    const cashIn = depositeNumber + currentAmount;
+    document.getElementById("current-balance").innerText = cashIn;
+    
+    //totalBalance
+    const savingsAmount=document.getElementById("total-balance").innerText;
+    const savingsNumber=parseFloat(savingsAmount);
+    const save=depositeNumber+savingsNumber;
+    document.getElementById("total-balance").innerText=save;
+
+    document.getElementById("deposite-amount").value="";
+})
+
+    //withdraw section
+    const cashOut = document.getElementById("cash-out");
+    cashOut.addEventListener("click",function(){
+
+    const withdrawEntry= document.getElementById("withdraw-entry").value;
+    const withdrawNumber=parseFloat(withdrawEntry);
+
+    const withdrawAmount= document.getElementById("withdraw-amount").innerText;
+    const withdrawnums= parseFloat(withdrawAmount);
+    const totalWithdraw= withdrawNumber+withdrawnums;
+    document.getElementById("withdraw-amount").innerText=totalWithdraw;
+    document.getElementById("withdraw-entry").value="";
+
+    //total withdraw
+    
+
+
+
+    
 })
